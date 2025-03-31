d0f950a3660d4255c5af31d66eec03653a16c316
git config --global u ser.name Rohan-Suryawanshi <suryawanshi9673r@gmail.com>
2025-03-31 10:30:09 +0530

    Refactor frontend setup; remove favicon, integrate Tailwind CSS, update index.html and README, and add Vite logo

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-
-
-   return (
-   <div>Hello React</div>
-   );
+  return (
+     <>
+        <h1 className="bg-amber-100">Hello world!</h1>
+     </>
+  );
 }
 
-export default App;
+export default App
