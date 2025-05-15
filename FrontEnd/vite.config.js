079e86f8d745819f0af31f92a635ca2d7118b168
git config --global u ser.name Rohan-Suryawanshi <suryawanshi9673r@gmail.com>
2025-05-15 18:49:38 +0530

    fix: Update API proxy URL in Vite configuration for production deployment

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
    server: {
       proxy: {
-         "/api": "http://localhost:3000",
+         // "/api": "http://localhost:3000",
+         "/api": "https://esport-x.vercel.app/",
       },
    },
    logLevel: "info", // Add this line
